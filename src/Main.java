028ff0e318d5748658014339357a62893eae5360
Thu Thuy Nguyen <nguyenthuthuy4218@gmail.com>
2016-06-22 03:04:19 +0700

    Day2

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        float f = sc.nextFloat();
+        System.out.println("C:" + ((f-32)/18000));
+
+    }
+}
